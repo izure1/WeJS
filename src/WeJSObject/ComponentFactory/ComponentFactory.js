6f20f623c8def2fe745b984301a39f780483287d
izure <=>
2020-01-05 18:07:26 +0900

    오타 수정

--- src/WeJSObject/ComponentFactory/ComponentFactory.js
+++ src/WeJSObject/ComponentFactory/ComponentFactory.js
@@ -6,7 +6,7 @@ class ComponentFactory {
   create(Reservation) {
     const data = new Reservation
     const builder = new ComponentBuilder
-    return builder.setName(data.name).setCache(data).build()
+    return builder.setName(data.name).setData(data).build()
   }
 
   createFromName(name) {
