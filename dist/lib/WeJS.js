f954b1b0e117d178b14ee7d2b3ad605808b5d854
izure <=>
2020-01-11 02:43:14 +0900

    오디오가 중복재생되는 오류를 수정

--- dist/lib/WeJS.js
+++ dist/lib/WeJS.js
@@ -40159,7 +40159,9 @@ __webpack_require__.r(__webpack_exports__);
 async function play(time = 0) {
   // 오디오가 로드되는 것을 기다립니다.
   await this.waitLoading();
+  this.audio.stop();
   this.audio.play();
+  this.audio.seek(time / 1000);
 }
 
 /***/ }),
